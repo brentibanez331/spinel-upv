8a50b0f91119fadb5ca7d1650e84ca5baa210f2c
PatrickJohn243 <florespatrickjohn243@gmail.com>
2025-04-11 09:05:03 +0800

    fix: f

--- lib/pdf-loader.ts
+++ lib/pdf-loader.ts
@@ -1,24 +1,29 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-
+import { UnstructuredLoader } from "@langchain/community/document_loaders/fs/unstructured";
 import { env } from "./config";
 
+export async function getChunkedDocsFromMarkdown() {
+  try {
+    const markdownPath = "../../../../README.md";
+    // const loader = new PDFLoader(env.PDF_PATH);
+    const loader = new UnstructuredLoader(markdownPath, {
+      apiKey: process.env.UNSTRUCTURED_API_KEY,
+      apiUrl: process.env.UNSTRUCTURED_API_URL,
+    });
 
-export async function getChunkedDocsFromPDF(){
-    try{
-        const loader = new PDFLoader(env.PDF_PATH);
-        const docs = await loader.load();
+    const docs = await loader.load();
 
-        const textSplitter = new RecursiveCharacterTextSplitter({
-            chunkSize: 1000,
-            chunkOverlap: 200
-        })
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
 
-        const chunkedDocs = await textSplitter.splitDocuments(docs);
+    const chunkedDocs = await textSplitter.splitDocuments(docs);
 
-        return chunkedDocs
-    }catch(e) {
-        console.error(e);
-        throw new Error("PDF docs chunking failed !")
-    }
+    return chunkedDocs;
+  } catch (e) {
+    console.error(e);
+    throw new Error("PDF docs chunking failed !");
+  }
 }
